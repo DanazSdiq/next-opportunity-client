06304790aaa1e8e4ca1ae341256873efd19a3e0d
danazsdiq <danazsdiq17@gmail.com>
2023-08-18 23:36:49 +0300

    feat: Displaying opportunity cards

---
+++ src/types/opportunity.ts
@@ -0,0 +1,11 @@
+export type Opportunity = {
+  id: string;
+  title: string;
+  organization_id: string;
+  description: string;
+  labels: string[];
+  commitment: string;
+  url: string;
+  created_at: string;
+  updated_at: string;
+};
