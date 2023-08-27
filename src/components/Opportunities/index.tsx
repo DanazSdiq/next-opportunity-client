0b7447751d7950478bfcedcf8efe84873d693980
danazsdiq <danazsdiq17@gmail.com>
2023-08-27 11:33:56 +0300

    feat: Changing the grid numbers in /opportunities

--- src/components/Opportunities/index.tsx
+++ src/components/Opportunities/index.tsx
@@ -21,7 +21,7 @@ export const Opportunitise = () => {
 
   return (
     <div
-      className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4 ${getDefaultPaddings()}`}
+      className={`${getDefaultPaddings()} grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2 md:gap-4`}
     >
       {data.map((opportunity) => (
         <OpportunityCard
